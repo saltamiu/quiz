88ec7f9e36f2d2a1dad884c8ee00d34fce54292b
iulichka <itvel11@freeuni.edu.ge>
2016-07-06 05:02:36 +0400

    servlet for quizes played by users

---
+++ src/model/HistoryItem.java
@@ -0,0 +1,22 @@
+package model;
+
+public class HistoryItem {
+	private User user;
+	private int score;
+	
+	public User getUser() {
+		return user;
+	}
+	
+	public void setUser(User user) {
+		this.user = user;
+	}
+	
+	public int getScore() {
+		return score;
+	}
+	
+	public void setScore(int score) {
+		this.score = score;
+	}
+}
